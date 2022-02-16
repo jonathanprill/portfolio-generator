49e764b71be6a32787401ca38e07f67b010aaa0b
jonyprill <jonathantprill@gmail.com>
2022-02-15 18:23:25 -0600

    inquirer added

--- app.js
+++ app.js
@@ -5,28 +5,38 @@
 //     for (let i = 0; i < profileDataArr.length; i+=1) {
 //         console.log(profileDataArr[i]);
 //     }
-
 //     console.log('===============');
-
 //     // Is the same as this...
 //     profileDataArr.forEach(profileItem => console.log(profileItem));
 // };
 
-
 // printProfileData(profileDataArgs);
 
-const fs = require('fs');
+// const profileDataArgs = process.argv.slice(2);
+// const [name, github] = profileDataArgs;
 
-const generatePage = require('./src/page-template.js');
+////////////////////////////////////////////////////////////////////////////////
 
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
 
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers))
