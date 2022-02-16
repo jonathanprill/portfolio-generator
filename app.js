2ff354987aee82c51f4fe5ee9509234c7cdb00ca
jonyprill <jonathantprill@gmail.com>
2022-02-16 15:24:05 -0600

    finished site

--- app.js
+++ app.js
@@ -20,7 +20,11 @@
 const { link } = require('fs');
 const inquirer = require('inquirer');
 
-const fs = require('fs');
+//const fs = require('fs');
+
+//This will import the exported object from generate-site.js
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+
 const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
@@ -165,15 +169,21 @@ const promptProject = portfolioData => {
   });
 };
 
-promptUser()
-  .then(promptProject)
+promptUser()         // We start by asking the user for their information with Inquirer prompts; this returns all of the data as an object in a Promise.
+  .then(promptProject)   // The promptProject() function captures the returning data from promptUser() and we recursively call promptProject() for as many projects as the user wants to add. Each project will be pushed into a projects array in the collection of portfolio information, and when we're done, the final set of data is returned to the next .then().
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-
-  });
+    return generatePage(portfolioData); // The finished portfolio data object is returned as portfolioData and sent into the generatePage() function, which will return the finished HTML template code into pageHTML.
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML); // We pass pageHTML into the newly created writeFile() function, which returns a Promise. This is why we use return here, so the Promise is returned into the next .then() method. 
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();   // Upon a successful file creation, we take the writeFileResponse object provided by the writeFile() function's resolve() execution to log it, and then we return copyFile().
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);  // The Promise returned by copyFile() then lets us know if the CSS file was copied correctly, and if so, we're all done!
+  })
+  .catch(err => {
+    console.log(err);
+});
